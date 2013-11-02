f027b59abe6b010d2c2b34f23ac2ebfed427e281
Iiro Tähkä <iiro.tahka@gmail.com>
2013-11-02 22:03:00 +0200

    Refines PCA -> Classification model

--- models/create_model_PCA.m
+++ models/create_model_PCA.m
@@ -1,7 +1,10 @@
-function [rtrn] = create_model_PCA_mvn(data, data_labels)
+function [deg, errors] = create_model_PCA(data, data_labels, classes)
     
-    degree_of_projections = 60;
+    degree_of_projections = 20;
     
+    % create a map for the classes
+    class_to_num = containers.Map(classes, 1:length(classes));
+
     % Init the indices to partition
     N = size(data, 1);
     parts = 200;
@@ -10,7 +13,6 @@ function [rtrn] = create_model_PCA_mvn(data, data_labels)
     
     errors = zeros(degree_of_projections, 2);
     for i=1:K
-
         % Partition the set
         validation_idx = find(indices == i);
         training_idx = find(indices ~= i);    
@@ -18,30 +20,43 @@ function [rtrn] = create_model_PCA_mvn(data, data_labels)
         validation_set = data(validation_idx, :);
         training_set = data(training_idx, :);
 
-        validation_labels = data_labels(validation_idx);
-        training_labels = data_labels(training_idx);
+        validation_labels = cellfun(@(x) class_to_num(x), ...
+        data_labels(validation_idx));
+        training_labels = cellfun(@(x) class_to_num(x), ...
+        data_labels(training_idx));
         
         [coeff, score] = pca(training_set);
-        
-
+       
         for d = 1:degree_of_projections
             new_d = score(:,1:d);
 
-            % Which way is thisi even supposed to be multiplied?
+            % Which way is this even supposed to be multiplied?
             class_t = classify(training_set*coeff(:, 1:d), new_d, ... 
             training_labels);
             class_v = classify(validation_set*coeff(:, 1:d), new_d, ...
             training_labels);
             
-            t_error = sum(strcmp(class_t, training_labels) == 0);
+            [~,~,~,~,Fscore] = getcm(training_labels, class_t, ...
+            1:length(classes));
 
-            v_error = sum(strcmp(class_v, validation_labels) == 0);
+            t_error = mean(Fscore);
 
-            errors(d, 1) = t_error;% + d / degree_of_projections * 0.5;
-            errors(d, 2) = v_error;% + d / degree_of_projections * 0.5;
+            [~,~,~,~,Fscore] = getcm(validation_labels, class_v, ...
+            1:length(classes));
+            Fscore
+            v_error = mean(Fscore);
+
+            %Magic!
+            alp = 0; 
+
+            errors(d, 1) = t_error + alp;
+            errors(d, 2) = v_error + alp;
         end
     end
-    errors = errors/K
+
+
+    errors = errors;
+    [minerr, deg] = min(errors(:,2));
     plot([1:degree_of_projections], errors(:,1), 'ro');
     hold on
     plot([1:degree_of_projections], errors(:,2), 'bo');
