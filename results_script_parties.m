6eaebae5f2c0dd7b814277a09438b167cfbe2cb9
vukk <unto.kuuranne@aalto.fi>
2013-11-04 18:23:14 +0200

    Fixes results_script_parties, changes evaluate to evaluate_2

--- results_script_parties.m
+++ results_script_parties.m
@@ -1,9 +1,9 @@
 clear all;
 
+data_load_script;
+
 select = 2;
 d = 13;
-
-data_load_script;
 
 [test_target_variables, test_target_names, test_data, test_feature_names] = ...
 load_HS_vaalikone('data/data_vk_test_full.csv');
@@ -16,4 +16,4 @@ target_variables(:,select));
 cls_v = cellfun(@(x) party_to_idx(x), cls);
 truth_v = cellfun(@(x) party_to_idx(x), test_target_variables(:,select));
 
-results = evaluate(cls_v, truth_v);
+results = evaluate_2(cls_v, truth_v);
